9f0f6682d975019eb721f24b56c8cbf67f538d69
Marcin Walewski <marcin.walewski@interia.pl>
2012-12-12 20:29:29 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rascaltiles::Application.config.secret_token = 'd5ea4a7310123634e145469f94633168222c6d3f60b0d6a5048ab740a054c35c2b443187c2f6adc85fc19e9ee508ee58c879df861b5f431129eddf67cf11de14'
